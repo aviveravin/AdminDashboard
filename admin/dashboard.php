2b2237610c9b1b4ce552b42bf99c22ca521eefe0
aviveravin <avinashverma165@gmail.com>
2018-09-04 02:01:16 +0530

    deleted defaults events

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -2,20 +2,12 @@
 include "header.php";
 
 ?>
-            
+
             <?php
             /* Content */
-            include "./content/event1.php";
             
-            include "./content/event2.php";
-            
-            include "./content/event3.php";
-            
-            include "./content/event4.php";
-
-            include "./content/event5.php";
             ?>
 
-<?php            
+<?php
 include "footer.php";
 ?>
